dd602e2e3a1ed148172befbd46a91ce83722b613
ZOTTCE <zottce@gmail.com>
2017-01-11 00:49:36 +0300

    fixed spaces

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ mod interpreter;
 use interpreter::default_opcodes::DefaultOpcodes;
 
 fn main() {
-    let bytecode: Vec<u8> = vec![0x01, 0x00, 0x05, 0xD0, 0x07, 0x02, 0x00, 0x0E, 0x05, 0x68, 0x65, 0x6C, 0x6C, 0x6F];
+	let bytecode: Vec<u8> = vec![0x01, 0x00, 0x05, 0xD0, 0x07, 0x02, 0x00, 0x0E, 0x05, 0x68, 0x65, 0x6C, 0x6C, 0x6F];
 
-    let mut cleo_vm = interpreter::VirtualMachine::new();
+	let mut cleo_vm = interpreter::VirtualMachine::new();
 
-    cleo_vm.set_default_opcodes();
-    cleo_vm.append_script(bytecode);
-    cleo_vm.run();
+	cleo_vm.set_default_opcodes();
+	cleo_vm.append_script(bytecode);
+	cleo_vm.run();
 }
